d1e92989034a6afae42ab7700430537f7c81fb1a
Xiao Gui <xgui3783@gmail.com>
2022-08-25 13:53:56 +0200

    add legend red vs white

--- viewer_plugin/src/ShowResult.svelte
+++ viewer_plugin/src/ShowResult.svelte
@@ -5,6 +5,14 @@
   <span slot="label">Annotate</span>
 </FormField>
 
+{#if redText && whiteText}
+<div>Legend</div>
+<ul>
+  <li style="color: red">{ redText }</li>
+  <li style="color: white">{ whiteText }</li>
+</ul>
+{/if}
+
 <script>
 import Switch from "@smui/switch"
 import FormField from '@smui/form-field'
@@ -15,6 +23,8 @@ const MNI152ID = "minds/core/referencespace/v1.0.0/dafcffc5-4826-4bf1-8ff6-46b8a
 export let postMessage = async (...arg) => { throw new Error(`postMessage need to be overriden by parent!`) }
 export let result;
 let checked = false
+let redText = null
+let whiteText = null
 
 let addedAnnotations = []
 
@@ -22,13 +32,20 @@ function handleSwitchEvent(event) {
   if (!result) throw new Error(`result is not defined`)
   const flag = event.detail.selected
   if (flag) {
+    result.mnicoords.forEach(({ roi }, index) => {
+      if (index === 0) {
+        redText = roi
+      } else {
+        whiteText = roi
+      }
+    })
     addedAnnotations = result.mnicoords.map(({ mnicoord, roi }, index) => {
       return mnicoord.map((coord, idx) => {
         return {
           '@id': `siibra-jugex-${roi}-${idx}`,
           name: `${roi}: ${idx}`,
           description: `${roi}: ${idx}: ${JSON.stringify(coord)}`,
-          color: index % 2 === 0 ? 'WHITE' : 'RED',
+          color: roi === whiteText ? 'WHITE' : 'RED',
           openminds: {
             coordinateSpace: {
               "@id": MNI152ID
